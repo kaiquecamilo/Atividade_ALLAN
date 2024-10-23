0ed866bc48e25b94cc0c21dc789d4cf408861d49
kaiquecamilo <kaique280806@edu.unipar.br>
2024-10-22 21:47:57 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const products = [
+  {
+    id: 1,
+    title: 'Smartphone',
+    price: 'R$ 1200,00',
+    image: 'https://s2-techtudo.glbimg.com/6AkJAf61Ocug01pj8QEJKOBB4zo=/0x0:696x436/984x0/smart/filters:strip_icc()/s.glbimg.com/po/tt2/f/original/2016/10/10/celular_da_xuxa.jpg',
+  },
+  {
+    id: 2,
+    title: 'Laptop',
+    price: 'R$ 3500,00',
+    image: 'https://candide.vtexassets.com/arquivos/ids/1373983-800-800?v=638586786181630000&width=800&height=800&aspect=true',
+  },
+  {
+    id: 3,
+    title: 'Headset',
+    price: 'R$ 250,00',
+    image: 'https://photos.enjoei.com.br/cd-player-karaoke-da-xuxa/1200xN/czM6Ly9waG90b3MuZW5qb2VpLmNvbS5ici9wcm9kdWN0cy84MzE3NDkxLzk1YjM0ZjdkOGMyYmFiNDNjNTc1ZTA5NjIyYmVjM2MyLmpwZw',
+  },
+];
+
+
+function App() {
+  return (
+    <div>
+      <h1>Lista de Produtos</h1>
+      <div style={{ display: 'flex', gap: '20px' }}>
+        {products.map((product) => (
+          <div key={product.id} style={{ border: '1px solid #ddd', padding: '10px', width: '200px' }}>
+            <img src={product.image} alt={product.title} style={{ width: '100%', height: '150px', objectFit: 'cover' }} />
+            <h2>{product.title}</h2>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
